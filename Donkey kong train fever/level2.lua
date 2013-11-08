f1c1794b042780c31abc240a5cb592179f35a90f
trond klakken <trond.klakken@bekk.no>
2013-11-08 15:09:32 +0100

    Banana!

--- Donkey kong train fever/level2.lua
+++ Donkey kong train fever/level2.lua
@@ -24,7 +24,7 @@ local screenW, screenH, halfW = display.contentWidth, display.contentHeight, dis
 -- 
 -----------------------------------------------------------------------------------------
 
-local ball
+local train
 local touch
 local cnt = 0
 local gr
@@ -43,6 +43,7 @@ function rail(group, x1, y1, x2, y2)
 	local railShape = { 0,0, x2-x1, y2-y1, x2-x1, y2-y1+10, 0, 10 }
 	rail:setReferencePoint( display.TopLeftReferencePoint )
 	physics.addBody( rail, "static", { shape=railShape } )
+	rail.type = "rail"
 	
 	group:insert(rail)
 end
@@ -63,9 +64,22 @@ function scene:createScene( event )
 	
 	physics.setDrawMode("hybrid")
 		
-	ball = display.newCircle( 10, 0, 10);
-	ball:setFillColor( 485 )
-	physics.addBody( ball, {density=1.0, friction=0, bounce=0, radius=10} )
+	train = display.newSprite( trainSprite(), { 
+		name = "normalRun",  --name of animation sequence
+	    start = 1,  --starting frame index
+	    count = 3,  --total number of frames to animate consecutively before stopping or looping
+	    time = 800,  --optional, in milliseconds; if not supplied, the sprite is frame-based
+	    loopCount = 0,  --optional. 0 (default) repeats forever; a positive integer specifies the number of loops
+	    loopDirection = "forward"  --optional, either "forward" (default) or "bounce" which will play forward then backwards through the sequence of frames
+	})  --if defining more sequences, place a comma here and proceed to the next sequence sub-table )
+	train.x, train.y = 58, 58
+	train.canJump = 0
+	train.type = "train"
+	train.collision = onLocalCollision
+	train:addEventListener( "collision", train )
+	train:play()
+
+	physics.addBody( train, {density=1.0, friction=0, bounce=0, radius=10} )
 
 	
 	rail(group, 0,200, 200,250)
@@ -73,14 +87,26 @@ function scene:createScene( event )
 	rail(group, 250,250, 300,200)
 	rail(group, 400,200, 500,250)
 	rail(group, 500,250, 800,250)
+
+	addBanana(group, 220, 230)
 	
-	group:insert(ball)
-	
-	local function onLocalCollision( self, event )
+	group:insert(train)
+
+	knapp:addEventListener( "touch", jumpAction )
+end
+
+function gameLoop(event)
+	gr.x = 0 - train.x + 100
+end
+
+Runtime:addEventListener("enterFrame", gameLoop)
+
+function onLocalCollision( self, event )
+	if event.other.type == "rail" then
 		if (event.phase == "began") then
 			physics.setGravity(0,0)
 			touch = event.other
-			ball:setLinearVelocity(300*touch.dx, 300*touch.dy)
+			train:setLinearVelocity(300*touch.dx, 300*touch.dy)
 			cnt = cnt+1
 			print("began " .. tostring(event.other))
 		end
@@ -92,25 +118,18 @@ function scene:createScene( event )
 			print("ended " .. tostring(event.other))
 		end
 	end
-	 
-	ball.collision = onLocalCollision
-	ball:addEventListener( "collision", ball )
-	knapp:addEventListener( "touch", jumpAction )
-end
+	if event.other.type == "banana" then
 
-function gameLoop(event)
-	gr.x = 0 - ball.x + 100
+	end
 end
-
-Runtime:addEventListener("enterFrame", gameLoop)
 
 function jumpAction(event)
 	print("HALLO")
 	local touchOrSpace = event.phase == "began" or (event.phase == "down" and event.keyName == "space")
    	if ( touchOrSpace and cnt > 0 ) then
-		local vx, vy = ball:getLinearVelocity()
-		ball:setLinearVelocity(vx, 0)
-    	ball:applyForce( 0, -180, ball.x, ball.y )
+		local vx, vy = train:getLinearVelocity()
+		train:setLinearVelocity(vx, 0)
+    	train:applyForce( 0, -180, train.x, train.y )
    	end
 end
 
@@ -136,6 +155,130 @@ function scene:destroyScene( event )
 	
 	package.loaded[physics] = nil
 	physics = nil
+end
+
+
+function addBanana(group, x , y, r)
+	-- create a grass object and add physics (with custom shape)
+	local banana = display.newSprite( bananaSprite(), { 
+		name = "normalRun",  --name of animation sequence
+	    start = 1,  --starting frame index
+	    count = 8,  --total number of frames to animate consecutively before stopping or looping
+	    time = 800,  --optional, in milliseconds; if not supplied, the sprite is frame-based
+	    loopCount = 0,  --optional. 0 (default) repeats forever; a positive integer specifies the number of loops
+	    loopDirection = "forward"  --optional, either "forward" (default) or "bounce" which will play forward then backwards through the sequence of frames
+	})
+	banana.x, banana.y = x, y
+	banana.type = "banana"
+	
+	-- define a shape that's slightly shorter than image bounds (set draw mode to "hybrid" or "debug" to see)
+	local bananaShape = { -7, -7, 7, -7, 7, 7, -7, 7 }
+	physics.addBody( banana, "kinematic", { friction=0, shape=bananaShape, isSensor = true } )
+
+	group:insert( banana)
+	banana:play()
+
+end
+
+function trainSprite()
+	return graphics.newImageSheet( "train.png", {
+	    --array of tables representing each frame (required)
+	    frames = {
+		        -- FRAME 1:
+		        {
+		            --all parameters below are required for each frame
+		            x = 0,
+		            y = 0,
+		            width = 55,
+		            height = 58
+		        },
+
+		        -- FRAME 2:
+		        {
+		            x = 56,
+		            y = 0,
+		            width = 55,
+		            height = 58
+		        },
+		        -- FRAME 3:
+		        {
+		            x = 113,
+		            y = 0,
+		            width = 55,
+		            height = 58
+		        },
+
+		        -- FRAME 3 and so on...
+		    },
+
+		    --optional parameters; used for dynamic resolution support
+		    sheetContentWidth = 165,
+		    sheetContentHeight = 58
+		})
+end
+
+function bananaSprite()
+	return graphics.newImageSheet( "banana.png", {
+	    --array of tables representing each frame (required)
+	    frames = {
+		        {
+		            x = 0,
+		            y = 0,
+		            width = 15,
+		            height = 16
+		        },
+
+		        {
+		            x = 15,
+		            y = 0,
+		            width = 15,
+		            height = 16
+		        },
+		        {
+		            x = 30,
+		            y = 0,
+		            width = 15,
+		            height = 16
+		        },
+		        {
+		            x = 45,
+		            y = 0,
+		            width = 15,
+		            height = 16
+		        },
+
+		        {
+		            x = 60,
+		            y = 0,
+		            width = 15,
+		            height = 16
+		        },
+		        {
+		            x = 75,
+		            y = 0,
+		            width = 15,
+		            height = 16
+		        },
+		        {
+		            x = 90,
+		            y = 0,
+		            width = 15,
+		            height = 16
+		        },
+
+		        {
+		            x = 105,
+		            y = 0,
+		            width = 15,
+		            height = 16
+		        }
+
+		    },
+
+		    --optional parameters; used for dynamic resolution support
+		    sheetContentWidth = 117,
+		    sheetContentHeight = 16
+		})
 end
 
 -----------------------------------------------------------------------------------------
